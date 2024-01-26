4efceca4b636698f413f6a1e20ab531cad46363b
Guillermo Uzner <90360692+guillermouzner@users.noreply.github.com>
2024-01-26 18:06:45 -0300

    Update pix.py

--- pix.py
+++ pix.py
@@ -6,30 +6,69 @@ async def validate_existence_pixKey(pixKey):
     validated_pixkey= formatPixKey(pixKey)
 
     if validated_pixkey [0] == True:
-        if len(pixKey) <= 36 or (len(pixKey) >= 36 and isMail(pixKey)):
-            try:
-                SPAPI = SmartPayApiAsync(API_USER,API_SECRET,API_URL)
-                postdata = {
-                "address": validated_pixkey[1] # pixkey
+        try:
+          if len(pixKey) <= 36 or (len(pixKey) >= 36 and isMail(pixKey)):
+            SPAPI = SmartPayApiAsync(API_USER,API_SECRET,API_URL)
+            postdata = {
+              "address": validated_pixkey[1] # pixkey
+            }
+            data = await SPAPI.sendApi("pix/checkkey", postdata)
+
+            if data.get('status') == 'ok':
+                return {
+                    "status_code": status.HTTP_200_OK, 
+                    "detail": {
+                        "msg": "valid pixkey", 
+                        "reformated_key": validated_pixkey[1], 
+                        "name": data['data']['name'], 
+                        "amount": ""
                     }
-                res = await SPAPI.sendApi("pix/checkkey", postdata)
+                }
 
-                if res['status']=='ok':
-                    return({"status_code": status.HTTP_200_OK, "detail": {"msg": "valid pixkey", "reformated_key": validated_pixkey[1], "name": res['data']['name'], "amount": ""}})
-                else:
-                    return({"status_code": status.HTTP_200_OK, "detail": {"msg": "pix key not found", "reformated_key": None, "name":None}} )
-
-            except:
-                return({"status_code": status.HTTP_500_INTERNAL_SERVER_ERROR, "detail": "an unexpected error ocurred"})
-            
-        else:
-            async with httpx.AsyncClient() as client:
-                url_parsed = quote(validated_pixkey[1])
-                response = await client.get(f"https://connect.smartpay.com.vc/api/pix/qrdecode?qrcode={url_parsed}")
-
-            if response.status_code == 200:
-                return({"status_code": status.HTTP_200_OK, "detail": {"msg": "valid pixkey", "reformated_key": validated_pixkey[1], "name": response.json()['data']['name'], "amount": str(response.json()['data']['amount'])}})
             else:
-                return({"status_code": status.HTTP_200_OK, "detail": {"msg": "pix key not found", "reformated_key": None, "name":None}} )
+                return {
+                    "status_code": status.HTTP_400_BAD_REQUEST, 
+                    "detail": {
+                        "msg": "pix key not found",
+                        "reformated_key": None, 
+                        "name": None
+                    }
+                  }
+
+          if len(pixKey) > 36:
+            async with httpx.AsyncClient() as client:
+              url_parsed = quote(validated_pixkey[1])
+              response = await client.get(f"https://connect.smartpay.com.vc/api/pix/qrdecode?qrcode={url_parsed}")
+
+              if response.status_code != 200:
+                raise Exception()
+
+              data = response.json()
+
+              if data.get('status') == 'ok':
+                return {
+                  "status_code": status.HTTP_200_OK, 
+                  "detail": {
+                      "msg": "valid pixkey", 
+                      "reformated_key": validated_pixkey[1], 
+                      "name": response.json()['data']['name'], 
+                      "amount": str(response.json()['data']['amount'])
+                  }
+                }
+
+              if data.get('status') == 'failed':
+                return {
+                    "status_code": status.HTTP_400_BAD_REQUEST, 
+                    "detail": {
+                        "msg": "Expired or Invalid QR code"
+                    }
+                  }
+        except Exception:
+          return {
+            "status_code": status.HTTP_500_BAD_REQUEST, 
+            "detail": {
+                "msg": "an unexpected error ocurred"
+            }
+          }
     else:
         return({"status_code": status.HTTP_200_OK, "detail":{"msg": validated_pixkey[1],"reformated_key": None, "name": None}})
